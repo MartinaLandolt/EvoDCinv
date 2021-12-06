2181df50d84f7c31b455f8772a9f557fde8e2ee1
Alexandre Kazantsev <alexandre.kazantsev@storengy.com>
2021-12-06 19:55:27 +0100

    continued scripts for model reading up to the problem of unconsistent coordinate extents between interface data and velocity data

--- make_synthetics/main_synthetics.py
+++ make_synthetics/main_synthetics.py
@@ -3,6 +3,7 @@ from make_synthetics import settings_synthetics
 from pathlib import Path
 import glob
 import pandas as pd
+import numpy as np
 
 
 def read_model_fmt1(path_model, nb_interfaces):
@@ -23,11 +24,62 @@ def read_model_fmt1(path_model, nb_interfaces):
     velocity_list = []
     thickness_list = []
 
+    # read all interfaces
     for (i, interface_file_i) in enumerate(interface_file_list):
         df_interface_i = pd.read_csv(path_model_depth.joinpath(interface_file_i))
         interface_list.append(df_interface_i)
+        interface_order.append(i)
 
-    return interface_list
+    # read all velocities
+    for (i, velocity_file_i) in enumerate(velocity_file_list):
+        df_velocity_i = pd.read_csv(path_model_vel.joinpath(velocity_file_i))
+        velocity_list.append(df_velocity_i)
+
+    # order interfaces by increasing depth
+    flag_order_correct = False
+    counter = -1
+    while (not flag_order_correct) & (counter < nb_interfaces**2) :
+        counter = counter + 1
+        print('number of layer reoderings: ', counter)
+        flag_order_correct = True
+        interface_list = [interface_list[j] for j in interface_order]
+        interface_i_minus_1 = interface_list[0]
+        vals_i_minus1 = interface_i_minus_1.iloc[:, -1].values
+        vals_i_minus1[vals_i_minus1==-9999.] = np.nan
+        for (i, interface_i) in enumerate(interface_list):
+            if i > 1:
+                vals_i = interface_i.iloc[:,-1].values
+                vals_i[vals_i==-9999.] = np.nan
+                diff_z = vals_i - vals_i_minus1
+                if np.nanmin(diff_z) < 0:
+                    if np.nanmax(diff_z) > 0:
+                        raise Exception("".join(["crossing interfaces : ", interface_i.columns.values[-1], " and ",
+                                                 interface_i_minus_1.columns.values[-1]]))
+                    flag_order_correct = False
+                    interface_order[i] = i - 1
+                    interface_order[i - 1] = i
+                interface_i_minus_1 = interface_i
+                vals_i_minus1 = vals_i_minus1
+
+    if counter == nb_interfaces**2:
+        raise Exception("something went wrong : the interface ordering counter reached max limit")
+
+    # get layer thickness & velocity
+    for (i, interface_i) in enumerate(interface_list):
+        vals_i = interface_i.iloc[:, -1].values
+        vals_i[vals_i == -9999.] = np.nan
+        if i > 1:
+            diff_z = vals_i - vals_i_minus1
+            velocity_i = velocity_list[i-1]
+            thickness_i = velocity_i.copy()
+            thickness_i.columns.values[-1].replace('Vint', 'Thickness')
+            thickness_i.iloc[:,-1] = diff_z
+            thickness_list.append(thickness_i)
+        interface_i_minus_1 = interface_i
+        vals_i_minus1 = interface_i_minus_1.iloc[:, -1].values
+        vals_i_minus1[vals_i_minus1 == -9999.] = np.nan
+
+    return interface_list, thickness_list, velocity_list
 
 
 def get_interface_number_fmt1(path_model):
@@ -82,7 +134,7 @@ if __name__ == '__main__':
     else:
         raise Exception("User-fixed layer number not yet supported. Number of layers should be auto")
 
-    interface_list = read_model_fmt1(path_model_in, n_interfaces)
+    interface_list, thickness_list, velocity_list = read_model_fmt1(path_model_in, n_interfaces)
 
     # loop_on_cells
 
