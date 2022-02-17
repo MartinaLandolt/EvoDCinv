3919e949f968b65dabcbaa1291c2b92ae19c628e
aliex <alexandre.kazantsev@gmail.com>
2022-02-17 14:43:32 +0100

    finished example_dcres_view_3d_vp_over_vs.py, tryed. put the best vp/vs model in settings.ini

--- examples/example_dcres_view_3d_vp_over_vs.py
+++ examples/example_dcres_view_3d_vp_over_vs.py
@@ -3,7 +3,9 @@ import sys, glob, fnmatch
 import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
+import matplotlib.gridspec as gridspec
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 import os
 import argparse
 
@@ -58,14 +60,14 @@ if __name__ == "__main__":
     colorby = 'misfit'
     # clim_mode = 'fixed'
     clim_mode = 'normalized'
-    misfit_bounds = [0, 1]
+    misfit_bounds = [0, 1.1]
     outdir = "output/" + output_name # Output directory
     fwd_modelling_file = outdir + "/fwd_modelling.pickle"
-    mean_model_file = outdir + "/mean_model_misfit_max_1.txt"
-    min_model_file = outdir + "/min_models_misfit_max_1.txt"
-    max_model_file = outdir + "/max_models_misfit_max_1.txt"
+    mean_model_file = outdir + "/mean_model_misfit_max_%s.txt" % str(misfit_bounds[-1])
+    min_model_file = outdir + "/min_models_misfit_max_%s.txt" % str(misfit_bounds[-1])
+    max_model_file = outdir + "/max_models_misfit_max_%s.txt" % str(misfit_bounds[-1])
     #best_model_file = outdir + "/best_model.pickle"
-    best_model_file = outdir + "/best_model_misfit_max_1.txt"
+    best_model_file = outdir + "/best_model_misfit_max_%s.txt" % str(misfit_bounds[-1])
     figdir = outdir + "/figures"  # Output figures directory
     cmap = "viridis_r"  # Colormap
     if not os.path.exists(figdir):
@@ -104,10 +106,10 @@ if __name__ == "__main__":
         apost_tomo_dcurves = dict_forward_modelling['misfit_table'][:, 1]
         forward_dcurves_global = dict_forward_modelling['forward_dcurves_global']
         forward_dcurves_tomo = dict_forward_modelling['forward_dcurves_tomo']
-        faxis_global = dict_forward_modelling['faxis_global'] # todo: reactivate this line after new run of dcres
+        faxis_global = dict_forward_modelling['faxis_global']
     # faxis_file = outdir + "/faxis_global.pickle"
     # with open(faxis_file, 'rb') as f1:
-        # faxis_global = pickle.load(f1) # todo: remove this line after new run of dcres
+        # faxis_global = pickle.load(f1) #
 
     # read models & energy
     with open(outdir + '/models.pickle', 'rb') as f1:
@@ -156,19 +158,33 @@ if __name__ == "__main__":
     # with open(best_model_file, 'rb') as f1:
         # best_model = pickle.load(f1)
 
-    # Import best model
-    # todo: with the new version of example dcres - read as text file
-    mean_true_model = np.nanmax(dispersion_dict_tomo['true_model'], axis=0)
-    mean_true_thickness = mean_true_model[:, -1]
+    # Import max layer thickness
+    max_true_model = np.nanmax(dispersion_dict_tomo['true_model'], axis=0)
+    max_true_thickness = max_true_model[:, -1]
+
+    # get Vs models
+    mean_true_model = np.nanmean(dispersion_dict_tomo['true_model'], axis=0)
+    mean_true_vp = mean_true_model[:, 0]
+    _, model_mean_vp_vals = make_plottable_model(max_true_thickness, mean_true_vp, zmax=zmax)
+    model_mean_vals_vs = model_mean_vp_vals/model_mean_plot[1, :]
+    model_min_vals_vs = model_mean_vp_vals/model_min_plot[1, :]
+    model_max_vals_vs = model_mean_vp_vals/model_max_plot[1, :]
+    model_best_vals_vs = model_mean_vp_vals/best_model_plot[1, :]
+
+    # get contrasts
+    model_mean_vals_contrasts = np.array(model_mean_vals_vs[:-2]) / np.array(model_mean_vals_vs[2:])
+    model_min_vals_contrasts = np.array(model_min_vals_vs[:-2]) / np.array(model_min_vals_vs[2:])
+    model_max_vals_contrasts = np.array(model_max_vals_vs[:-2]) / np.array(model_max_vals_vs[2:])
+    model_best_vals_contrasts = np.array(model_best_vals_vs[:-2]) / np.array(model_best_vals_vs[2:])
     # best_model_vals = best_model.model_vp_over_vs
-    # _, model_best_vals = make_plottable_model(mean_true_thickness, best_model_vals, zmax=zmax)
+    # _, model_best_vals = make_plottable_model(max_true_thickness, best_model_vals, zmax=zmax)
 
     # Plot all models colored by apost relative to different misfits
     fig1 = plt.figure()
     fig1.patch.set_alpha(0.)
     ax1 = fig1.add_subplot(1, 1, 1)
     for i in range(n_models):
-        z_plot, model_plot = make_plottable_model(mean_true_thickness, models[i, :], zmax=zmax)
+        z_plot, model_plot = make_plottable_model(max_true_thickness, models[i, :], zmax=zmax)
         ax1.plot(model_plot, z_plot, color=smap.to_rgba(color_vals[i]))
     h_mean, = ax1.plot(model_mean_vals, z_plot, color='k', lw=2)
     h_best, = ax1.plot(model_best_vals, z_plot, color='r', ls='--', lw=2)
@@ -184,6 +200,53 @@ if __name__ == "__main__":
     plt.tight_layout()
     pos_ax1 = ax1.get_position()
     plt.savefig("%s/models_with_%s.png" % (figdir, colorby))
+    plt.close(fig1)
+    
+    # plot Vs models
+    fig1 = plt.figure()
+    fig1.patch.set_alpha(0.)
+    ax1 = fig1.add_subplot(1, 1, 1)
+    for i in range(n_models):
+        z_plot, model_plot = make_plottable_model(max_true_thickness, mean_true_vp/models[i, :], zmax=zmax)
+        ax1.plot(model_plot, z_plot, color=smap.to_rgba(color_vals[i]))
+    h_mean, = ax1.plot(model_mean_vals_vs, z_plot, color='k', lw=2)
+    h_best, = ax1.plot(model_best_vals_vs, z_plot, color='r', ls='--', lw=2)
+    ax1.set_ylim(z_plot[0], z_plot[-1])
+    ax1.invert_yaxis()
+    ax1.set_xlabel("Vs (m/s)", fontsize=12)
+    ax1.set_ylabel("Depth (m)", fontsize=12)
+    ax1.grid(True, linestyle=":")
+    cb1 = fig1.colorbar(smap)
+    cb1.set_label(colorby, fontsize=12)
+    ax1.legend([h_mean, h_best], ['weighted average model', 'best model'],
+               bbox_to_anchor=(1.3, 1), loc='upper left')
+    plt.tight_layout()
+    pos_ax1 = ax1.get_position()
+    plt.savefig("%s/models_Vs_with_%s.png" % (figdir, colorby))
+    plt.close(fig1)
+
+    # plot Vs contrasts
+    fig1 = plt.figure()
+    fig1.patch.set_alpha(0.)
+    ax1 = fig1.add_subplot(1, 1, 1)
+    for i in range(n_models):
+        z_plot, model_plot = make_plottable_model(max_true_thickness, mean_true_vp/models[i, :], zmax=zmax)
+        model_contrast = np.array(model_plot[:-2])/np.array(model_plot[2:])
+        ax1.plot(model_contrast, z_plot[:-2], color=smap.to_rgba(color_vals[i]))
+    h_mean, = ax1.plot(model_mean_vals_contrasts, z_plot[:-2], color='k', lw=2)
+    h_best, = ax1.plot(model_best_vals_contrasts, z_plot[:-2], color='r', ls='--', lw=2)
+    ax1.set_ylim(z_plot[0], z_plot[-1])
+    ax1.invert_yaxis()
+    ax1.set_xlabel("Contrast Vs(n)/Vs(n+1)", fontsize=12)
+    ax1.set_ylabel("Depth (m)", fontsize=12)
+    ax1.grid(True, linestyle=":")
+    cb1 = fig1.colorbar(smap)
+    cb1.set_label(colorby, fontsize=12)
+    ax1.legend([h_mean, h_best], ['weighted average model', 'best model'],
+               bbox_to_anchor=(1.3, 1), loc='upper left')
+    plt.tight_layout()
+    pos_ax1 = ax1.get_position()
+    plt.savefig("%s/models_Vs_contrast_with_%s.png" % (figdir, colorby))
     plt.close(fig1)
 
     # Plot model envelopes
@@ -203,6 +266,45 @@ if __name__ == "__main__":
     plt.tight_layout()
     ax1.set_position(pos_ax1)
     plt.savefig("%s/models_with_envelopes.png" % figdir)
+    plt.close(fig1)
+
+    # Plot model envelopes
+    fig1 = plt.figure()
+    fig1.patch.set_alpha(0.)
+    ax1 = fig1.add_subplot(1, 1, 1)
+    h_area = ax1.fill_betweenx(z_plot, model_min_vals_vs, model_max_vals_vs, color='gray', alpha=0.5)
+    h_mean, = ax1.plot(model_mean_vals_vs, z_plot, color='k')
+    h_best, = ax1.plot(model_best_vals_vs, z_plot, color='r', ls='--')
+    ax1.set_ylim(z_plot[0], z_plot[-1])
+    ax1.invert_yaxis()
+    ax1.set_xlabel("Vs (m/s)", fontsize=12)
+    ax1.set_ylabel("Depth (m)", fontsize=12)
+    ax1.grid(True, linestyle=":")
+    ax1.legend([h_area, h_mean, h_best], ['range of models', 'weighted average model', 'best model'],
+               bbox_to_anchor=(1.3, 1), loc='upper left')
+    plt.tight_layout()
+    ax1.set_position(pos_ax1)
+    plt.savefig("%s/models_Vs_with_envelopes.png" % figdir)
+    plt.close(fig1)
+
+    # plot Vs contrasts envelopes
+    fig1 = plt.figure()
+    fig1.patch.set_alpha(0.)
+    ax1 = fig1.add_subplot(1, 1, 1)
+    h_area = ax1.fill_betweenx(z_plot[:-2], model_min_vals_contrasts,
+                               model_max_vals_contrasts, color='gray', alpha=0.5)
+    h_mean, = ax1.plot(model_mean_vals_contrasts, z_plot[:-2], color='k')
+    h_best, = ax1.plot(model_best_vals_contrasts, z_plot[:-2], color='r', ls='--')
+    ax1.set_ylim(z_plot[0], z_plot[-1])
+    ax1.invert_yaxis()
+    ax1.set_xlabel("Contrast Vs(n)/Vs(n+1)", fontsize=12)
+    ax1.set_ylabel("Depth (m)", fontsize=12)
+    ax1.grid(True, linestyle=":")
+    ax1.legend([h_area, h_mean, h_best], ['range of models', 'weighted average model', 'best model'],
+               bbox_to_anchor=(1.3, 1), loc='upper left')
+    plt.tight_layout()
+    ax1.set_position(pos_ax1)
+    plt.savefig("%s/models_Vs_contrast_with_envelopes.png" % figdir)
     plt.close(fig1)
 
     # detect how many wave types there are in global data
@@ -289,28 +391,46 @@ if __name__ == "__main__":
     for i, (x, y) in enumerate(zip(x_tomo_all, y_tomo_all)):
         # get geographical coords, prepare file name for figure
         str_title = 'group_dispersion_curves_tomo_cell_X_' + str(int(x)) + '_Y_' + str(int(y))
-        fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-
+        fig = plt.figure(figsize=(8, 6))
+        gs = gridspec.GridSpec(1, 2, width_ratios=[2, 1])
+        ax1 = plt.subplot(gs[0])
+        ax2 = plt.subplot(gs[1])
         # plot forward modelled curves
         for (dcurve_forward_list, a) in zip(forward_dcurves_tomo, color_vals):
             dcurve_plot = dcurve_forward_list[i]
-            ax.set_title("Rayleigh - tomography - cell X = " + str(int(x)) + '; Y = ' + str(int(y)))
-            ax.plot(faxis_global, dcurve_plot, color=smap.to_rgba(a))
+            ax1.set_title("Rayleigh - tomography - cell X = " + str(int(x)) + '; Y = ' + str(int(y)))
+            ax1.plot(faxis_global, dcurve_plot, color=smap.to_rgba(a))
 
         # plot data
-        h_data = ax.errorbar(dispersion_dict_tomo['f_axis'], dispersion_dict_tomo['DispersionCurves'][i].dtype_velocity,
+        h_data = ax1.errorbar(dispersion_dict_tomo['f_axis'], dispersion_dict_tomo['DispersionCurves'][i].dtype_velocity,
                     yerr=dispersion_dict_tomo['DispersionCurves'][i].uncertainties, color='k',
                              fmt="o", elinewidth=1, capsize=1.5, ms=3, mew=0.3,
                              zorder=10, alpha=0.7)
-        ax.legend([h_data], ['Tomography - mode 0'], loc='upper right')
-        cb1 = fig.colorbar(smap)
+
+        ax1.legend([h_data], ['Tomography - mode 0'], loc='upper right')
+        divider = make_axes_locatable(ax1)
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        cb1 = fig.colorbar(smap, cax=cax, orientation='vertical')
+        #cb1 = ax1.colorbar(smap)
         cb1.set_label(colorby, fontsize=12)
-        ax.set_ylabel(f"{dispersion_dict_tomo['DispersionCurves'][i].dtype} velocity (m/s)", fontsize=12)
-        ax.set_xlabel("Frequency (Hz)", fontsize=12)
-        ax.set_xlim(min(faxis_global), max(faxis_global))
-        ax.set_ylim(vmin_group, vmax_group)
-        ax.grid(True, linestyle=":")
-        plt.tight_layout()
+        ax1.set_ylabel(f"{dispersion_dict_tomo['DispersionCurves'][i].dtype} velocity (m/s)", fontsize=12)
+        ax1.set_xlabel("Frequency (Hz)", fontsize=12)
+        ax1.set_xlim(min(faxis_global), max(faxis_global))
+        ax1.set_ylim(vmin_group, vmax_group)
+        ax1.grid(True, linestyle=":")
+
+        # plot misfit map and indicate where the cell is
+        ax2.scatter(x_tomo_all/1000, y_tomo_all/1000, color='k', s=5)
+        ax2.scatter(x/1000, y/1000, color='r', s=20)
+        ax2.set_aspect('equal', 'box')
+        ax2.set_title("Location in \n tomography grid")
+        ax2.set_xlabel('X (km)')
+        ax2.set_ylabel('Y (km)')
+        ax2.grid(True, linestyle=":")
+        ax2.set_xticks(np.arange(int(min(x_tomo_all/1000)), int(max(x_tomo_all/1000))) + 1)
+        ax2.set_yticks(np.arange(int(min(y_tomo_all/1000)), int(max(y_tomo_all/1000))) + 1)
+
+        plt.tight_layout(pad=3.0)
         fig.savefig(figdir + f'/{str_title}.png', dpi=400)
         plt.close(fig)
 
